07bdaf435ab30b14d8a04f0ceb7f39b0d2b2d581
Rinni Swift <rinradaswift@gmail.com>
2019-01-24 10:33:04 -0800

    radomly rearanges users terminal input

--- python_quote.py
+++ python_quote.py
@@ -1,4 +1,5 @@
 import random
+import sys
 
 quotes = ("It's just a flesh wound.", 
 		  "He's not the Messiah. He's a very naughty boy!",
@@ -8,6 +9,29 @@ def random_python_quote():
 	rand_ind = random.randint(0, len(quotes) - 1)
 	return quotes[rand_ind]
 
+
+def rearange_input():
+	'''
+		append all users input items into a list
+		randomly append item to a new array and remove it from the one you are looping through
+		OR random.randint
+	'''
+	array = []
+	for i in range(1, len(sys.argv)):
+		array.append(sys.argv[i])
+
+
+	result_array = []
+	for i in range(1, len(sys.argv)):
+		item = random.choice(array)
+		result_array.append(item)
+		array.remove(item)
+	return result_array
+
+
+
 if __name__ == '__main__':
 	quote = random_python_quote()
 	print(quote)
+	rearange = rearange_input()
+	print(rearange)
