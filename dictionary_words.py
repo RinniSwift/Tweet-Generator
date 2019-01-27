4f04ea26b00cfefee443c62bf287512bde8b2a62
Rinni Swift <rinradaswift@gmail.com>
2019-01-26 18:43:28 -0800

    file to randomize words from the dictionary file on the desktop to generate sentences with n amount of words

---
+++ dictionary_words.py
@@ -0,0 +1,5 @@
+'''
+* read in the words file '/usr/share/dict/words'
+* select a random set of words from the file and store in a data type
+* put the number of words requested together into a "sentence"
+* output your sentence'''
