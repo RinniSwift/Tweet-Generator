232ba179196518965b8ac13fa300d6d1c07928f9
Rinni Swift <rinradaswift@gmail.com>
2019-01-30 13:12:56 -0800

    gives a list of words that match the terminal input

--- autocomplete.py
+++ autocomplete.py
@@ -1,30 +1,24 @@
+import sys
+
 
 def autocomplete():
 
-    teamnames=["Blackpool","Blackburn","Arsenal"]
+    with open('/usr/share/dict/words', 'r') as f:
+        file_content = f.read()
+    lines = file_content.split()
 
-    user_input = input("Your choice: ")
+    matched_items = []
+    letter = sys.argv[1]
 
-    # You have to handle the case where 2 or more teams starts with the same string.
-    # For example the user input is 'B'. So you have to select between "Blackpool" and
-    # "Blackburn"
-    filtered_teams = lambda x: x.startswith(user_input), teamnames
-
-    if len(filtered_teams) > 1:
-        # Deal with more that one team.
-        print('There are more than one team starting with "{0}"'.format(user_input))
-        print('Select the team from choices: ')
-        for index, name in enumerate(filtered_teams):
-            print("{0}: {1}".format(index, name))
-
-        index = input("Enter choice number: ")
-        # You might want to handle IndexError exception here.
-        print('Selected team: {0}'.format(filtered_teams[index]))
-
-    else:
-        # Only one team found, so print that team.
-        print(filtered_teams[0])
+    while (len(matched_items) != 10):
+        for word in lines:
+            if word.startswith(letter):
+                matched_items.append(word)
+            if len(matched_items) == 10:
+                break
+        
+    return matched_items
 
 
 if __name__ == "__main__":
-    autocomplete()
+    print(autocomplete())
