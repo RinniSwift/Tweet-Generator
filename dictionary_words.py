012bb52f4c127f7463e869fcf94f400465029234
Rinni Swift <rinradaswift@gmail.com>
2019-02-09 12:16:13 -0800

    update

--- dictionary_words.py
+++ dictionary_words.py
@@ -9,29 +9,16 @@ import random
 
 def generate_sentence_from_dict(file):
 	# get the number of words passed in as the terminal input
-	word_count = sys.argv[1]							
-	string = ""
+	word_count = sys.argv[1]
 
 	# use the 'with' keyword to make sure you close the file after
 	with open(file, 'r') as f:							
-		file_content = f.read()
-
-	# split all words in the file			
+		file_content = f.read()			
 	lines = file_content.split()
 
 	words = random.choices(lines, k=int(word_count))
+
 	return " ".join(words)
-
-
-
-	# loop through n amount of times to add words to the string
-	# for i in range(0, int(word_count)):
-	# 	# randomize a number from 0 to the length of the file word count					
-	# 	random_line = random.randrange(0, len(lines))
-	# 	# append the files index to the string	
-	# 	string += (lines[random_line] + " ")			
-
-	# return string
 
 
 def sentence_form(string):
@@ -41,3 +28,5 @@ def sentence_form(string):
 if __name__ == "__main__":
 	generate_words = generate_sentence_from_dict('/usr/share/dict/words')
 	print(sentence_form(generate_words))
+
+
