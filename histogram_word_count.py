b6ef3960e037d1ec548f5b8631c2199667ebc183
Rinni Swift <rinradaswift@gmail.com>
2019-02-09 11:57:08 -0800

    better implementation

--- histogram_word_count.py
+++ histogram_word_count.py
@@ -1,42 +1,35 @@
+from string import punctuation
 
 def file_to_word_array(file):
+	'''function that changes a text file to an array of words excluding all punctuations'''
 
 	with open(file, 'r') as f:
 		text_content = f.read()
-	words = text_content.lower().strip("(),!.?""/").split()
-	return words
+	words = text_content.lower()
+	word_arr_stripped = ''.join(c for c in words if c not in punctuation).split()
+	return word_arr_stripped
 
 
-def histogram_dict(file):
 
+def histogram_dict(words):
+	'''returns a dictionary as the key being the type and the value being the tokens'''
 	# DICTIONARY APPROACH O(n)
 
 	histogram = {}
 
-	with open(file, 'r') as f:
-		text_content = f.read()
-
-	words = text_content.lower().strip("(),!.?""/").split()
-
-	# loop through all words in the text file and check if the word contains in the histogram, make the count 1, or else add 1 to the previous count
 	for word in words:
 		if word in histogram.keys():
 			histogram[word] += 1
 		else:
 			histogram[word] = 1
 
-	# return a histogram with the count of word frequency
 	return histogram
 
-def histogram_list_of_lists(file):
-
+def histogram_list_of_lists(words):
+	'''returns a list of the words and amount of times it has appeared.'''
 	# LIST APPROACH O(n^2)
 
 	histogram = []
-
-	with open(file, 'r') as f:
-		file_content = f.read()
-	words = file_content.split()
 
 	for word in words:
 		word_found = False
@@ -51,14 +44,13 @@ def histogram_list_of_lists(file):
 	return histogram
 
 
-def histogram_list_of_tuples(list_):
-
+def histogram_list_of_tuples(words):
+	'''returns a list of tuples with the type and token'''
 	# TUPLE APPROACH O(n^2)
 
 	tuple_histogram = []
-
 	
-	for word in list_:
+	for word in words:
 		found = False
 		for item in tuple_histogram:
 			if word == item[0]:
@@ -73,42 +65,14 @@ def histogram_list_of_tuples(list_):
 	return tuple_histogram
 
 
-def unique_words(histogram):
-	unique_count = 0
-	for key, value in histogram.items():
-		if value == 1:
-			unique_count += 1
-	return unique_count
-
-
-def frequency(word, histogram):
-	return histogram[word]
-
-
-def most_frequent_count(histogram):
-	# use the reduce function to compare all values of the histogram and return the key/keys with the largest values.
-	current_largest = 1
-	most_frequent_word = ""
-	for key, value in histogram.items():
-		if value >= current_largest:
-			current_largest = value
-			most_frequent_word = key
-
-	return ("Most frequent word: \"" + most_frequent_word + "\", "+ str(current_largest) + " times")
-
-def list_of_counts():
-	#implement the dictionary and reverse to list of counts
-	# loop through dict in dictionary, add the dict[1] item to the first index in the new_tuple_list
+def list_of_counts(dictionary):
+	'''returns a list of of tuples where the item at [0] is the tokens and item at [1] is a list of types which have that amount of tokens'''
+	# implement a dictionary. loop through dictionary, add the dict[1] item to the first index in the new_tuple_list
 	# as (dict[1], [dict[0]]). if dict[1] is in new_tuple_list, append the dict[0] in the tuple value
 
-	dictionary = {'one': 1, 'blue': 1, 'two': 1, 'fish': 4, 'red': 1}
+	# dictionary = {'one': 1, 'blue': 1, 'two': 1, 'fish': 4, 'red': 1}
 
 	new_tuple_list = []
-
-	# new_tuple_list.append((1, ["one"]))
-	# for item in new_tuple_list:
-	# 	if item[0] == 1:
-	# 		item[1].append("hi")
 
 
 	for key, value in dictionary.items():
@@ -127,17 +91,42 @@ def list_of_counts():
 	return new_tuple_list
 
 
+def unique_words(list_counts):
+	'''return the amount of unique words in the file and return the words'''
+	unique_count = 0
+	for item in list_counts:
+		if item[0] == 1:
+			counts_of_one = len(item[1])
+			return (counts_of_one)
+			break
+
+
+def frequency(word, histogram):
+	return histogram[word]
+
+
+def most_frequent_count(histogram):
+	# use the reduce function to compare all values of the histogram and return the key/keys with the largest values.
+	current_largest = 1
+	most_frequent_word = ""
+	for key, value in histogram.items():
+		if value >= current_largest:
+			current_largest = value
+			most_frequent_word = key
+
+	return ("Most frequent word: \"" + most_frequent_word + "\", "+ str(current_largest) + " times")
+
+
+
 if __name__ == "__main__":
 
-	# word_array = file_to_word_array('BreakfastAtTiffanys.txt')
-	# print(histogram_dict('BreakfastAtTiffanys.txt'))
-	# print("frequency of word the word \"love\": ", frequency('love', histogram('BreakfastAtTiffanys.txt')))
-	# print("unique word count: ", unique_words(histogram('BreakfastAtTiffanys.txt')))
-	# print(most_frequent_count(histogram('BreakfastAtTiffanys.txt')))
-	# print(histogram_list_of_lists('BreakfastAtTiffanys.txt'))
+	word_array = file_to_word_array('BreakfastAtTiffanys.txt')
+	# print(word_array)
+	dictio = histogram_dict(word_array)
+	# print(dictio)
+	# print(histogram_list_of_lists(word_array))
 	# print(histogram_list_of_tuples(word_array))
-	print(list_of_counts())
-
-
-
-# create a class called histogram and put the functions in
+	# print(list_of_counts(dict))
+	# print("frequency of word the word \"love\": ", frequency('love', dictio))
+	# print("unique word count: ", unique_words(list_of_counts(dictio)))
+	# print(most_frequent_count(dictio))
