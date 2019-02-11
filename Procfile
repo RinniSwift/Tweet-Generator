8913b577c873a30c78228c6e71966a70ec138f91
Rinni Swift <rinradaswift@gmail.com>
2019-02-11 14:29:32 -0800

    update heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app.py --log-file=-
+web: gunicorn -w 4 app:app
--- app.py
+++ app.py
@@ -4,8 +4,3 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!' 
-
-
-@app.route('/favicon.ico')
-def hello_something():
-	return 'Hello something'
