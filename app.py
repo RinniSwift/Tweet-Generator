a8157450e03f1b0664523b29e244c3731a9fd862
Rinni Swift <rinradaswift@gmail.com>
2019-03-03 15:26:02 -0800

    import markov random generator functions file

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+import markov
+
 app = Flask(__name__)
+
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, World!' 
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!' 
+	return markov.generate_sentence(markov.first_markov_chain(markov.sentence_to_word_list(markov.read_file('/Users/rinniswift/dev/CS1.2/BreakfastAtTiffanys.txt'))))
