5a3cdaa1f03575307937719431b063bac64460b5
Rinni Swift <rinradaswift@gmail.com>
2019-03-03 15:30:33 -0800

    clean code

--- app.py
+++ app.py
@@ -9,4 +9,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return markov.generate_sentence(markov.first_markov_chain(markov.sentence_to_word_list(markov.read_file('/Users/rinniswift/dev/CS1.2/BreakfastAtTiffanys.txt'))))
+	file_split = file_to_word_list('/Users/rinniswift/dev/CS1.2/BreakfastAtTiffanys.txt')
+	dictionary = markov.first_markov_chain(file_split)
+	return markov.generate_sentence(dictionary)
+	
