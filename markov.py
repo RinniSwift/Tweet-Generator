99b08857ece9eb8745db15071c42412158dab4bd
Rinni Swift <rinradaswift@gmail.com>
2019-03-04 13:59:31 -0800

    new function with o(n) time for adding items to the inner dictionary of the markov chain

--- markov.py
+++ markov.py
@@ -18,7 +18,6 @@ def first_markov_chain(listt):
 	(key) as the keys/words in the sentence
 	(value) as a dictionary containg words that come after the (key)
 	'''
-	'''listt: one fish two fish red fish blue fish'''
 
 	markov = {}
 
@@ -28,20 +27,35 @@ def first_markov_chain(listt):
 			markov[item] = {}
 
 
-	index = 0
-	while index <= len(listt) - 2:	# indexing all indexes before the last index
+	# index = 0
+	# while index <= len(listt) - 2:	# indexing all indexes before the last index
+	# 	curr_word = listt[index]
+	# 	next_word = listt[index + 1]
+
+	# 	for word_pair in markov.items():
+	# 		if curr_word == word_pair[0]:	# check if curr_word in dictionaries key
+	# 			value = word_pair[1]		# set value variable to the dictionaries value at that key
+	# 			if next_word in value:
+	# 				value[next_word] += 1	# word has come after that key before, increment its count by 1
+	# 			else:
+	# 				value[next_word] = 1	# word has not come after that key, store the count of 1
+	# 	index += 1
+	# return markov
+
+	for index in range(0, (len(listt) - 1)):
 		curr_word = listt[index]
 		next_word = listt[index + 1]
 
-		for word_pair in markov.items():
-			if curr_word == word_pair[0]:	# check if curr_word in dictionaries key
-				value = word_pair[1]		# set value variable to the dictionaries value at that key
-				if next_word in value:
-					value[next_word] += 1	# word has come after that key before, increment its count by 1
-				else:
-					value[next_word] = 1	# word has not come after that key, store the count of 1
-		index += 1
+		value = markov[curr_word]
+
+		if next_word in value:
+			value[next_word] += 1
+		else:
+			value[next_word] = 1
 	return markov
+
+
+
 	'''
 	{'one': {'fish': 1}, 
 	 'fish': {'two': 1, 'red': 1, 'blue': 1}, 
