d1f8d7abe29043e3f9aba3dec4c882af20e23d86
Rinni Swift <rinradaswift@gmail.com>
2019-03-04 19:33:17 -0800

    read from corpus text

--- app.py
+++ app.py
@@ -7,9 +7,8 @@ app = Flask(__name__)
 # @app.route('/')
 # def hello_world():
 #     return 'Hello, World!' 
-app.file_split = markov.file_to_word_list('BreakfastAtTiffanys.txt')
+app.file_split = markov.file_to_word_list('corpus.txt')
 app.dictionary = markov.first_markov_chain(app.file_split)
 @app.route('/')
 def hello_world():
 	return markov.generate_sentence(app.dictionary)
-	
